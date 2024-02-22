71fb7c86797a8ed0745b871435c4ba8fc59376d9
Brian Kamotho <kariukibriankamotho@gmail.com>
2024-02-22 15:49:04 +0300

    initial commit

---
+++ store/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+import datetime
+# Create your models here.
+# categories of products
+class Category(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+    
+# customers
+class Customer(models.Model):
+    
+    first_name = models.CharField(max_length = 155)
+    last_name = models.CharField(max_length = 155)
+    phone = models.CharField(max_length = 15)
+    email = models.EmailField(max_length = 100)
+    password = models.CharField(max_length = 100)    
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+# products
+
+class Product(models.Model):
+    name =models.CharField(max_length = 155)
+    price = models.DecimalField(default = 0, decimal_places = 2, max_digits = 6)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, default = 1) 
+    description = models.CharField(max_length = 255, default = '', null = True, blank = True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    # add sale stuff
+    is_sale = models.BooleanField(default = False)
+    sale_price = models.DecimalField(default = 0, decimal_places = 2, max_digits = 6)
+    
+
+    def __str__(self):
+        return self.name
+
+# orders
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    cusomer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    quantity = models.IntegerField(default = 1)
+    address = models.CharField(max_length = 255, default = '', null = True, blank = True)
+    phone = models.CharField(max_length = 50, default = '', null = True, blank = True)
+    date = models.DateField(default = datetime.datetime.today)
+    status = models.BooleanField(default = False)
+
