ed60ff7a4f5c1b84147e7f55ef0099168aec4dff
Brian Kamotho <kariukibriankamotho@gmail.com>
2024-02-23 20:20:25 +0300

    LOGIN AND LOGOUT functionality

--- store/views.py
+++ store/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import  SignUpForm
+from django import forms
+
 
 # Create your views here.
+
+
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {
@@ -16,8 +23,44 @@ def about(request):
 
 
 def login_user(request):
-    return render(request, 'login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('you have been logged in'))
+            return redirect('home')
+
+        else:
+            messages.success(request, ('invalid'))
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html', {})
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'you have been logged out')
+    return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, 'Registration is successful, welcome ')
+            return redirect('home')
+
+        else:
+            messages.success(request, 'Whoops problem registering, please try again')
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
